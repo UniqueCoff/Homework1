422f63797dfb8593cbef20a809a1d0629836cca6
UniqueCoff <61749149+UniqueCoff@users.noreply.github.com>
2020-04-10 19:03:13 +0800

    Add files via upload

---
+++ Diapozon.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>﻿
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+
+    srand(time(NULL));
+
+    int c;
+
+    cout << "enter range" << endl;
+    cin >> c;
+
+
+    for (int i = 0; i < c; i++) {
+
+
+
+        int a = rand() % 20 - 10;
+
+        cout << a << endl;
+
+    }
+
+}
