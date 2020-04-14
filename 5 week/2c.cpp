b38a803c6f648d1681c985ebd624d63e2a4e601d
UniqueCoff <61749149+UniqueCoff@users.noreply.github.com>
2020-04-14 20:21:00 +0800

    Add files via upload

---
+++ 5 week/2c.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    
+    srand(time(NULL));
+
+    int const size = 10;
+    int c = 0;
+    int arr[size];
+
+    for (int i = 0; i < size; i++) {
+
+        arr[i] = rand() % 20 - 10;
+
+    }
+
+    for (int i = 0; i < size; i++) {
+
+        if (arr[i] > 3) {
+
+            c = arr[i] + c;
+
+        }
+
+    }
+
+    cout << c << endl;
+
+}
+
